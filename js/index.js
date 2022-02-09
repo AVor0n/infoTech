a37164e4a39ce3cc985686dd28258985173ec369
Andrey Voronin <AVor0n@yandex.ru>
2022-02-10 00:23:59 +0500

    feat: add entries editor

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import users from "../data/users.json" assert { type: "json" };
 const countRows = 10;
 let currentPage = 1;
 const countPages = Math.ceil(users.length / countRows);
+let selectedEntry;
 
 init();
 goToPage(currentPage);
@@ -44,6 +45,7 @@ function init() {
 
   currentPageIndicator.addEventListener("change", (e) => {
     const incomeValue = e.target.value;
+    if (incomeValue === currentPage) return;
 
     prevPageBtn.disabled = false;
     nextPageBtn.disabled = false;
@@ -61,17 +63,37 @@ function init() {
     goToPage(currentPage);
     currentPageIndicator.value = currentPage;
   });
+
+  const firstName = document.getElementById("first-name");
+  const lastName = document.getElementById("last-name");
+  const about = document.getElementById("about");
+  const eyeColor = document.getElementById("eye-color");
+
+  [firstName, lastName, about, eyeColor].forEach((input) =>
+    input.addEventListener("change", updateField)
+  );
 }
 
 function addRow(number, rowData) {
   const table = document.querySelector(".table__body");
   const row = document.createElement("tr");
+  row.classList.add("table__row");
 
   row.append(createTableCell(number));
   row.append(createTableCell(rowData.name.firstName));
   row.append(createTableCell(rowData.name.lastName));
   row.append(createTableCell(rowData.about, "about-cell"));
   row.append(createTableCell(rowData.eyeColor));
+
+  row.addEventListener("click", () => {
+    selectedEntry = number - 1;
+    showEditor(selectedEntry);
+
+    for (const r of table.children) {
+      r.classList.remove("active");
+    }
+    row.classList.add("active");
+  });
 
   table.append(row);
 }
@@ -104,4 +126,48 @@ function goToPage(pageNumber) {
   for (let i = startIdx; i < endIdx; i++) {
     addRow(i + 1, users[i]);
   }
+
+  hideEditor();
+}
+
+function hideEditor() {
+  const form = document.querySelector(".form");
+  form.hidden = true;
+}
+
+function showEditor(entryIdx) {
+  const form = document.querySelector('.form')
+  form.hidden = false;
+
+  const user = users[entryIdx];
+
+  const firstName = document.getElementById("first-name");
+  const lastName = document.getElementById("last-name");
+  const about = document.getElementById("about");
+  const eyeColor = document.getElementById("eye-color");
+
+  firstName.value = user.name.firstName;
+  lastName.value = user.name.lastName;
+  about.textContent = user.about;
+  eyeColor.value = user.eyeColor;
+}
+
+function updateField(event) {
+  const input = event.target;
+
+  switch (input.id) {
+    case "first-name":
+      users[selectedEntry].name.firstName = input.value;
+      break;
+    case "last-name":
+      users[selectedEntry].name.lastName = input.value;
+      break;
+    case "about":
+      users[selectedEntry].about = input.textContent;
+      break;
+    case "eye-color":
+      users[selectedEntry].eyeColor = input.value;
+      break;
+  }
+  goToPage(currentPage);
 }
